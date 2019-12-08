995e4b503b3744c05bfd076fc506370c3cc05f5f
Dai Kawahara <skbsporta7@gmail.com>
2019-12-08 17:20:02 +0900

    sepalate directory

---
+++ RxSwift-Memo-Sample/Presentation/ViewModel/MemoDetail/MemoDetailViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  MemoDetailViewModel.swift
+//  RxSwift-Memo-Sample
+//
+//  Created by kawaharadai on 2019/11/24.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+class MemoDetailViewModel {
+
+    let startSaveMemo: Driver<Memo>
+    let completeSaveMemo: Observable<Notification>
+
+    init(memo: Memo?, textViewText: Driver<String>, memoDataStore: MemoDataStoreNew, tappedDone: Signal<()>) {
+        // タップアクション → メモ作成 or メモ更新
+        startSaveMemo = tappedDone.withLatestFrom(textViewText)
+            .flatMapLatest({ (text) -> Driver<Memo> in
+                if let memo = memo {
+                    return memoDataStore.updateMemo(memo: memo, text: text).asDriver(onErrorJustReturn: memo)
+                } else {
+                    let createMemo = memoDataStore.createMemo(text: text)
+                    return createMemo.asDriver(onErrorJustReturn: Memo())
+                }
+            })
+
+        // メモ保存完了
+        completeSaveMemo = NotificationCenter.default.rx.notification(.NSManagedObjectContextDidSave)
+    }
+    
+}
